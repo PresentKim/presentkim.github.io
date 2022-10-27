730f04aff2b4141988e979c781c62374a6dc7939
PresentKim <debe3721@gmail.com>
2022-10-28 01:29:12 +0900

    :bug: Fix blog links in Nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -55,6 +55,6 @@
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+		<li><a rel=prefetch aria-current="{segment === 'posts' ? 'page' : undefined}" href="posts">posts</a></li>
 	</ul>
 </nav>
