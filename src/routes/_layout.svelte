2df20bfc399f85826c07a9e3802280220afde69e
PresentKim <debe3721@gmail.com>
2022-10-30 12:53:06 +0900

    :sparkles: Apply highlight.js and set color themes

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,12 +2,36 @@
   import '../global.css';
 
   import Nav from '../components/Nav.svelte';
+  import hljs from 'highlight.js/lib/core';
+  import javascript from 'highlight.js/lib/languages/javascript';
+  import java from 'highlight.js/lib/languages/java';
+  import kotlin from 'highlight.js/lib/languages/kotlin';
+  import php from 'highlight.js/lib/languages/php';
+  import cpp from 'highlight.js/lib/languages/cpp';
+
+  /** @type {Record<string, import('highlight.js').LanguageFn>} */
+  const languages = {js: javascript, javascript, java, kotlin, php, cpp};
+  for (const [name, language] of Object.entries(languages)) {
+    hljs.registerLanguage(name, language);
+  }
+  import {onMount} from 'svelte';
 
   export let segment;
 </script>
 
 <Nav {segment}/>
+  onMount(() => {
+    document.querySelectorAll('pre code').forEach((el) => {
+      el.innerHTML = el.innerHTML.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+      console.log(el.innerHTML);
+      hljs.highlightElement(el);
+    });
+  });
+</script>
 
+{#if segment !== undefined }
+    <Header/>
+{/if}
 <main>
     <slot></slot>
 </main>
