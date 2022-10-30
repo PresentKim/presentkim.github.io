8a3f27d5d1c0a69a9bb784b26680d4733121e42b
PresentKim <debe3721@gmail.com>
2022-10-31 00:41:18 +0900

    :construction: Create a main page temporarily

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,32 @@
-<h1>My blog</h1>
+<script>
+  let innerWidth = 0;
+  let innerHeight = 0;
+
+  $: vmin = Math.min(innerWidth, innerHeight) / 100;
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    p {
+        font-size: 1em;
+    }
+</style>
+
+<svelte:window bind:innerWidth={innerWidth} bind:innerHeight={innerHeight}/>
+
+<div class="container"
+     style:font-size=" {vmin * 2}px">
+
+    <h1>PresentKim</h1>
+    <p>My first paragraph.</p>
+
+    <ul>
+        <li><a href="posts">Posts</a></li>
+    </ul>
+</div>
