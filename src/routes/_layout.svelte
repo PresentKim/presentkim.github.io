e00f4f8e774e97f6e5119fe58c2f2c31a9f6c7a0
PresentKim <debe3721@gmail.com>
2022-10-30 22:16:33 +0900

    :bug: Fix font setting failures

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import '../assets/global.css';
+  import '../global.css';
 
   import hljs from 'highlight.js/lib/core';
   import javascript from 'highlight.js/lib/languages/javascript';
