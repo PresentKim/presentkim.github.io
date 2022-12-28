47bed9ce8128445fc51494a797f7419265ef1a55
PresentKim <debe3721@gmail.com>
2022-12-29 01:51:04 +0900

    :sparkles: Add `/boj/[id]` page (Boj problem preview page)

---
+++ src/routes/boj/[id]/+page.ts
@@ -0,0 +1,11 @@
+import type { BojInfo } from '$lib/utils/boj';
+import { error } from '@sveltejs/kit';
+
+export async function load({ fetch, params }: LoadArguments) {
+  const response = await fetch(`/api/boj/${params.id}`);
+  const info = (await response.json()) as BojInfo;
+  if (!info) {
+    throw error(404, 'Not found');
+  }
+  return info;
+}
