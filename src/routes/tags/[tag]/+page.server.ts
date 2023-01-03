f40330e3a75ef0f446a64c67be27cf3faaabae2a
PresentKim <debe3721@gmail.com>
2023-01-04 00:52:46 +0900

    :ambulance: Fix tags fetch url

--- src/routes/tags/[tag]/+page.server.ts
+++ src/routes/tags/[tag]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PostMetadata } from '$lib/components/blog';
 
 export async function load({ url, fetch, params }: LoadArguments) {
   const { tag } = params;
-  const response = await fetch(`${url.origin}/blog/index`);
+  const response = await fetch(`${url.origin}/api/blog/index`);
   const allPosts = await response.json();
 
   return {
