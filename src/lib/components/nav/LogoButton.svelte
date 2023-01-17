12a2e59501248fc26254cd7b17f166b5f0f6f58e
PresentKim <debe3721@gmail.com>
2023-01-17 17:00:55 +0900

    :lipstick: Makes Long logo to short than before
    <PRESENT KIM/> :arrow_right: <PRESENT/>

--- src/lib/components/nav/LogoButton.svelte
+++ src/lib/components/nav/LogoButton.svelte
@@ -2,14 +2,14 @@
   export let shorten: boolean = false;
 </script>
 
-<div class="select-none overflow-hidden w-60 max-xs:w-54 {shorten ? 'shorten w-8 max-xs:w-6' : ''}">
+<div class="select-none overflow-hidden w-44 max-xs:w-32 {shorten ? 'shorten w-8 max-xs:w-6' : ''}">
   <a href="/" data-sveltekit-preload-data="hover">
     <svg class="h-8 max-xs:h-6 my-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 315 42">
       <g class="fill-none stroke-round stroke-[6px]">
         <g id="code-wrap" class="stroke-emerald-500">
           <path class="translate-x-[000px]" d="M16,06 04,20 16,36" />
-          <path class="translate-x-[244px]" d="M04,36 16,06" />
-          <path class="translate-x-[268px]" d="M04,06 16,20 04,36" />
+          <path class="translate-x-[170px]" d="M04,36 16,06" />
+          <path class="translate-x-[190px]" d="M04,06 16,20 04,36" />
         </g>
         <g id="name-chars" class="stroke-current">
           <path class="translate-x-[044px]" d="M04,36 04,06 18,06 18,20 04,20 13,20 18,36" />
@@ -19,9 +19,6 @@
           <path class="translate-x-[110px]" d="M18,06 04,06 04,21 18,21 04,21 04,36 18,36" />
           <path class="translate-x-[132px]" d="M04,36 04,06 18,36 18,06" />
           <path class="translate-x-[154px]" d="M04,06 18,06 11,06 11,36" />
-          <path class="translate-x-[182px]" d="M04,06 04,36M04,26 18,06M04,16 18,36" />
-          <path class="translate-x-[204px]" d="M04,06 04,36" />
-          <path class="translate-x-[212px]" d="M04,36 04,06 14,36 24,06 24,36" />
         </g>
       </g>
     </svg>
@@ -35,20 +32,17 @@
   }
 
   .shorten {
-    #code-wrap path {
-      @apply translate-x-[-22px];
+    path {
+      @apply translate-x-[002px] translate-y-[002px] stroke-[#0000] -skew-y-6;
     }
 
     #name-chars path {
-      @apply translate-x-[-28px];
-      @apply -skew-y-6;
-
       &:nth-child(1) {
-        @apply translate-x-[002px] translate-y-[002px] stroke-emerald-900;
+        @apply stroke-emerald-900;
       }
 
       &:nth-child(2) {
-        @apply translate-x-[002px] translate-y-[002px] stroke-emerald-500;
+        @apply stroke-emerald-500;
       }
     }
   }
