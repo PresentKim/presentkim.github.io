3cfc5a709b5b2d0310266eed226ac8e7c2db6728
PresentKim <debe3721@gmail.com>
2023-01-17 17:34:13 +0900

    :lipstick: Makes Post card summary size up

--- src/lib/components/PostList.svelte
+++ src/lib/components/PostList.svelte
@@ -13,7 +13,7 @@
         <h4 class="text-xl font-bold">
           {post.title}
         </h4>
-        <p class="my-3 text-sm overflow-hidden text-ellipsis line-clamp-3">{post.summary}</p>
+        <p class="my-3 text-base overflow-hidden text-ellipsis line-clamp-3">{post.summary}</p>
 
         <p class="flex text-sm text-right text-neutral-550 dark:text-neutral-350">
           {post.formattedDate}
