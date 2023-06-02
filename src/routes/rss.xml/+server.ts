37e5d7699bc5689161495d6d9f30980fa8bf2740
PresentKim <debe3721@gmail.com>
2023-06-02 16:58:35 +0900

    :mag: Add robot.txt, sitemap.xml, and rss.xml

---
+++ src/routes/rss.xml/+server.ts
@@ -0,0 +1,41 @@
+import { getBlogPosts, getBlogPostByPermalink } from '$lib/utils/blog';
+import { title, domain, description } from '$lib/assets/site-info.json';
+import type { PostMetadata } from '$lib/utils/blog';
+export const prerender = true;
+
+export async function GET() {
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <rss
+        xmlns:dc="https://purl.org/dc/elements/1.1/"
+        xmlns:content="https://purl.org/rss/1.0/modules/content/"
+        xmlns:atom="https://www.w3.org/2005/Atom"
+        version="2.0">
+        <channel>
+            <title>${title}</title>
+            <link>${domain}</link>
+            <description>${description}</description>
+            ${(await getBlogPosts()).map((post) => item(post)).join(' ')}
+        </channel>
+    </rss>`.trim(),
+    {
+      headers: {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'max-age=0, s-maxage=3600'
+      }
+    }
+  );
+}
+
+function item({ title, permalink, summary, date }: PostMetadata) {
+  return `
+    <item>
+        <title>${title}</title>
+        <link>${domain}/${permalink}</link>
+        <guid isPermaLink="false">${domain}/${permalink}</guid>
+        <description>${summary}</description>
+        <pubDate>${date}</pubDate>
+        <!--TODO: include <content:encoded> tag-->
+    </item>
+`;
+}
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,40 @@
+import { getBlogPosts, domain } from '$lib/utils/blog';
+
+export const prerender = true;
+
+export async function GET() {
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+    >
+        ${(await getBlogPosts()).map(url).join(' ')}
+        ${url('portfolio')}
+        ${url('posts')}
+    </urlset>`.trim(),
+    {
+      headers: {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'max-age=0, s-maxage=3600'
+      }
+    }
+  );
+}
+
+function url(post: { permalink: string; date: string }): string;
+function url(permalink: string): string;
+function url(url: { permalink: string; date: string } | string): string {
+  return `
+    <url>
+        <loc>${domain}/${typeof url === 'object' ? url.permalink : url}</loc>
+        <changefreq>daily</changefreq>
+        ${typeof url === 'object' ? `<lastmod>${url.date}</lastmod>` : ''}
+        <priority>0.7</priority>
+    </url>
+`;
+}
