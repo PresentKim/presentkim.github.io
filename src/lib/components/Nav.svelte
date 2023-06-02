6f58606fdb6739edb665f53d9dbf9ac0b836bfc3
PresentKim <debe3721@gmail.com>
2023-06-02 11:53:22 +0900

    :sparkles: Add read progress bar to top

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,16 +3,29 @@
   import Hamburger from '$lib/components/nav/Hamburger.svelte';
   import ThemeButton from '$lib/components/nav/ThemeButton.svelte';
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
 
   let menuOpened;
   let widthEnough;
   let screenWidth = 0;
+
+  let readProgress = 0.0;
 
   //For makes logo to long when (screen width > sm(640px))
   $: widthEnough = screenWidth > 640;
 
   //Close hamburger menu when page redirected
   page.subscribe(() => (menuOpened = false));
+
+  onMount(() => {
+    function updateReadProgress() {
+      readProgress =
+        Math.min(1, window.pageYOffset / (document.body.scrollHeight - window.innerHeight)) * 100;
+      requestAnimationFrame(() => updateReadProgress());
+    }
+
+    window.requestAnimationFrame(updateReadProgress);
+  });
 </script>
 
 <svelte:window bind:innerWidth={screenWidth} />
@@ -24,6 +37,12 @@
          bg-white dark:bg-neutral-900 !bg-opacity-90
          border-b border-neutral-200 dark:border-neutral-800"
 >
+  <div class="w-screen">
+    <div
+      class="w-0 h-0.5 bg-neutral-700 dark:bg-neutral-200 rounded"
+      style="width: {readProgress}%;"
+    />
+  </div>
   <div
     class="blog-container flex justify-between px-2 py-3
            [&_*]:transition-shape [&_*]:duration-500 [&_*]:ease-spring"
