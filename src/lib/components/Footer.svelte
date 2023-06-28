0aeb0de87bebc61fb836798b3473bf102bd761c1
PresentKim <debe3721@gmail.com>
2023-06-28 17:13:29 +0900

    :lipstick: Remove powered links from footer, And add more nav links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,34 +1,51 @@
 <footer
   class="text-neutral-550 dark:text-neutral-350 bg-neutral-200 dark:bg-neutral-900 mx-auto mt-3 py-3"
 >
-  <nav class="flex flex-row justify-center text-center gap-4 my-4">
+  <nav class="flex flex-row justify-center text-center gap-5 mb-2">
+    <a
+      href="mailto:contanct@present.kim"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="김현재의 이메일"
+      class="hover:text-blue-500 dark:hover:text-blue-400"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6">
+        <g class="fill-none stroke-current stroke-round stroke-2">
+          <path d="M3 7a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7z" />
+          <path d="m3 7 9 6 9-6" />
+        </g>
+      </svg>
+    </a>
     <a
       href="https://github.com/PresentKim"
       target="_blank"
-      rel="noreferrer"
-      aria-label="김현재의 깃헙 페이지로 이동"
+      rel="noopener noreferrer"
+      aria-label="김현재의 깃허브"
+      class="hover:text-blue-500 dark:hover:text-blue-400"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 fill-current">
-        <path
-          d="M22 12.2a10 10 0 0 1-6.8 9.5c-.5.1-.7-.2-.7-.4v-2.8a2.4 2.4 0 0 0-.7-1.8c2.2-.3 4.6-1.1 4.6-5a3.9 3.9 0 0 0-1-2.7 3.6 3.6 0 0 0-.1-2.6s-.9-.3-2.8 1a9.5 9.5 0 0 0-5 0c-2-1.3-2.8-1-2.8-1a3.6 3.6 0 0 0 0 2.6 3.9 3.9 0 0 0-1 2.7c0 3.9 2.3 4.7 4.5 5a2.1 2.1 0 0 0-.7 1.3 2.1 2.1 0 0 1-2.9-.8 2.1 2.1 0 0 0-1.5-1s-1 0 0 .6a2.6 2.6 0 0 1 1 1.4s.6 2 3.4 1.4v1.7c0 .2-.2.5-.7.4A10 10 0 1 1 22 12.2Z"
-        />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6">
+        <g class="fill-none stroke-current stroke-round stroke-2">
+          <path
+            d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21"
+          />
+        </g>
+      </svg>
+    </a>
+    <a
+      href="/rss.xml"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="블로그 RSS XML"
+      class="hover:text-blue-500 dark:hover:text-blue-400"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6">
+        <g class="fill-none stroke-current stroke-round stroke-2">
+          <path d="M4 19a1 1 0 1 0 2 0 1 1 0 1 0-2 0M4 4a16 16 0 0 1 16 16M4 11a9 9 0 0 1 9 9" />
+        </g>
       </svg>
     </a>
   </nav>
-  <div class="flex flex-col justify-center text-center gap-5">
-    <div class="flex flex-wrap justify-center gap-x-2">
-      <p>© 2023 PresentKim.</p>
-      <p>All Rights Reserved.</p>
-    </div>
-
-    <div class="flex flex-wrap justify-center gap-x-2">
-      <p>Powered by</p>
-      <p>
-        <a href="https://pages.github.com/" target="_blank" rel="noreferrer">github pages</a>
-      </p>
-      <p><a href="https://kit.svelte.dev/" target="_blank" rel="noreferrer">sveltekit</a></p>
-
-      <p><a href="https:/tailwindcss.com" target="_blank" rel="noreferrer">tailwindcss</a></p>
-    </div>
+  <div class="text-sm text-center">
+    <p>© 2023 PresentKim. All Rights Reserved.</p>
   </div>
 </footer>
