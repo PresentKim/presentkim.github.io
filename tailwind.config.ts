a8086ceb39fc8c85c32776257bfc7a09b2dfefc3
PresentKim <debe3721@gmail.com>
2023-09-01 23:25:26 +0900

    :lipstick: Makes font more small

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,33 @@ module.exports = {
     scrollVariantsPlugin,
     transitionDurableMixinPlugin,
     variablePlugin,
-    require('tailwindcss-fluid-type')
+    require('tailwindcss-fluid-type')({
+      settings: {
+        fontSizeMin: 1.0, //  1.00rem === 16px
+        fontSizeMax: 1.25, // 1.25rem === 20px
+        ratioMin: 1.0, // Multiplication Min
+        ratioMax: 1.25, // Multiplication Max
+        screenMin: 17, //   17rem ===  272px
+        screenMax: 120, // 120rem === 1920px
+        unit: 'rem',
+        prefix: '',
+        extendValues: true
+      },
+      values: {
+        xs: [-2, 1.6],
+        sm: [-1, 1.6],
+        base: [0, 1.6],
+        lg: [1, 1.6],
+        xl: [2, 1.2],
+        '2xl': [3, 1.2],
+        '3xl': [4, 1.2],
+        '4xl': [5, 1.1],
+        '5xl': [6, 1.1],
+        '6xl': [7, 1.1],
+        '7xl': [8, 1],
+        '8xl': [9, 1],
+        '9xl': [10, 1]
+      }
+    })
   ]
 } satisfies Config;
