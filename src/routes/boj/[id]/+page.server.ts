08a864acc6737e8ec7ecbc6e77ef740aefe642b4
PresentKim <debe3721@gmail.com>
2024-04-03 16:15:21 +0900

    :fire: Remove useless `boj` data

--- src/routes/boj/[id]/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-import { getBojInfo } from '$lib/utils/boj';
-
-export const load: PageServerLoad = ({ params: { id } }) =>
-  getBojInfo(Number(id));
